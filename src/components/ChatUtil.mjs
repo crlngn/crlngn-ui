272161f65625d341eb4d0df0dafc0142e6d395a3
Caroline Rozendo <carolinex@gmail.com>
2025-03-02 19:37:51 -0800

    fix damage icon and cleanup

--- src/components/ChatUtil.mjs
+++ src/components/ChatUtil.mjs
@@ -12,16 +12,6 @@ export class ChatUtil {
       const rollType = chatMessage.flags?.dnd5e?.activity?.type || chatMessage.flags?.dnd5e?.roll?.type || "custom";
       let elem = html.get ? html.get(0) : html;
 
-      const locBtnPath = 'CRLNGN_UI.dnd5e.chatCard.buttons';
-      elem.style.setProperty('--crlngn-i18n-attack', game.i18n.localize(`${locBtnPath}.attack`));
-      elem.style.setProperty('--crlngn-i18n-damage', game.i18n.localize(`${locBtnPath}.damage`));
-      elem.style.setProperty('--crlngn-i18n-summons', game.i18n.localize(`${locBtnPath}.summons`));
-      elem.style.setProperty('--crlngn-i18n-healing', game.i18n.localize(`${locBtnPath}.healing`));
-      elem.style.setProperty('--crlngn-i18n-template', game.i18n.localize(`${locBtnPath}.template`));
-      elem.style.setProperty('--crlngn-i18n-consume', game.i18n.localize(`${locBtnPath}.consume`));
-      elem.style.setProperty('--crlngn-i18n-refund', game.i18n.localize(`${locBtnPath}.refund`));
-      elem.style.setProperty('--crlngn-i18n-macro', game.i18n.localize(`${locBtnPath}.macro`));
-      elem.style.setProperty('--crlngn-i18n-save-dc', game.i18n.localize(`${locBtnPath}.savedc`));
       elem.classList.add('crlngn');
       elem.classList.add(rollType);      
     }    
--- src/components/Main.mjs
+++ src/components/Main.mjs
@@ -21,6 +21,8 @@ export class Main {
       // Main.setupKeyListeners(); 
       SettingsUtil.registerSettings();
       Hooks.on(HOOKS_CORE.RENDER_CHAT_MESSAGE, Main.#onRenderChatMessage); 
+
+      Main.addCSSLocalization();
     });
 
     Hooks.once(HOOKS_CORE.READY, () => {
@@ -29,7 +31,6 @@ export class Main {
 
       // Get the array of available font families
       const availableFonts = CONFIG.fontFamilies || [];
-
 
       // If you need the complete font settings including URLs
       const fontSettings = SettingsUtil.get("fonts", "core");
@@ -40,10 +41,21 @@ export class Main {
       CameraUtil.init(); 
       PlayersListUtil.init(); 
       LeftControls.init();
-
-      LogUtil.log("GAME", [game.collections.find(c=>c.key=="Scene"),game.collections.get("Scene"), ui]); 
     })
-    
+  }
+
+  static addCSSLocalization(){
+    const body = document.querySelector('body');
+    const locBtnPath = 'CRLNGN_UI.dnd5e.chatCard.buttons';
+    body.style.setProperty('--crlngn-i18n-attack', game.i18n.localize(`${locBtnPath}.attack`));
+    body.style.setProperty('--crlngn-i18n-damage', game.i18n.localize(`${locBtnPath}.damage`));
+    body.style.setProperty('--crlngn-i18n-summons', game.i18n.localize(`${locBtnPath}.summons`));
+    body.style.setProperty('--crlngn-i18n-healing', game.i18n.localize(`${locBtnPath}.healing`));
+    body.style.setProperty('--crlngn-i18n-template', game.i18n.localize(`${locBtnPath}.template`));
+    body.style.setProperty('--crlngn-i18n-consume', game.i18n.localize(`${locBtnPath}.consume`));
+    body.style.setProperty('--crlngn-i18n-refund', game.i18n.localize(`${locBtnPath}.refund`));
+    body.style.setProperty('--crlngn-i18n-macro', game.i18n.localize(`${locBtnPath}.macro`));
+    body.style.setProperty('--crlngn-i18n-save-dc', game.i18n.localize(`${locBtnPath}.savedc`));
   }
 
   static setupKeyListeners(){
