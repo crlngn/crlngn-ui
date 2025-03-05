da66c23cee184e0aaa692397c341ef6ed0d78a6e
Caroline Rozendo <carolinex@gmail.com>
2025-03-04 20:21:30 -0800

    Update ChatUtil.mjs
    removed unnecessary tyle

--- src/components/ChatUtil.mjs
+++ src/components/ChatUtil.mjs
@@ -14,7 +14,7 @@ export class ChatUtil {
       const rollType = chatMessage.flags?.dnd5e?.activity?.type || chatMessage.flags?.dnd5e?.roll?.type || "custom";
       let elem = html.get ? html.get(0) : html;
 
-      elem.classList.add('crlngn');
+      // elem.classList.add('crlngn');
       elem.classList.add(rollType);
       LogUtil.log("enrichCard", [chatMsgSettings.borderColor, BORDER_COLOR_TYPES.playerColor.name, chatMessage.author?.id]); 
 
