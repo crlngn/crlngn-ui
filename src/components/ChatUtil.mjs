4b72a2e8aebf5f4f3b8fd51bd8c68eaf97ffad99
Caroline Rozendo <carolinex@gmail.com>
2025-03-18 20:39:34 -0700

    Merge fixes for visible/hidden dc with midi-qol

--- src/components/ChatUtil.mjs
+++ src/components/ChatUtil.mjs
@@ -12,21 +12,47 @@ export class ChatUtil {
     ChatUtil.chatBorderColor = SettingsUtil.get(SETTINGS.chatBorderColor.tag);
   }
 
-  static enrichCard = (chatMessage, html) => {
+  static enrichCard = async(chatMessage, html) => {
     LogUtil.log("renderChatMessage", [ChatUtil.chatBorderColor, ChatUtil.enableChatStyles, BORDER_COLOR_TYPES.playerColor.name, chatMessage.author?.id]); 
+    const rollType = chatMessage.flags?.dnd5e?.activity?.type || chatMessage.flags?.dnd5e?.roll?.type || "custom";
+    let chatItem = html.get ? html.get(0) : html;
+    chatItem.classList.add(rollType);
 
-    if(ChatUtil.enableChatStyles){ 
-      const rollType = chatMessage.flags?.dnd5e?.activity?.type || chatMessage.flags?.dnd5e?.roll?.type || "custom";
-      let elem = html.get ? html.get(0) : html;
+    // Create a temporary div to parse the HTML content of message
+    const content = duplicate(chatMessage.content);
+    const tempElement = document.createElement('div');
+    tempElement.innerHTML = content;
 
-      // elem.classList.add('crlngn');
-      elem.classList.add(rollType);
+    if(ChatUtil.enableChatStyles && !tempElement.firstChild.classList.contains('crlngn')){ 
+      let elem;
+      elem = tempElement.firstChild;
+      elem.classList.add('crlngn');
 
+      const saveButtons = tempElement.querySelectorAll('.card-buttons button[data-action=rollSave]');
+      LogUtil.log("enrichCard",[saveButtons.length]);
+      if (saveButtons.length > 0) {      
+        saveButtons.forEach(button => {
+          const visibleDCSpan = button.querySelector('.visible-dc');
+          const hiddenDCSpan = button.querySelector('.hidden-dc');
+          LogUtil.log("enrichCard",[visibleDCSpan, hiddenDCSpan]);
+          visibleDCSpan.setAttribute('data-ability', button.getAttribute('data-ability') || "");
+          visibleDCSpan.setAttribute('data-dc', button.getAttribute('data-dc') || "");
+          hiddenDCSpan.setAttribute('data-ability', button.getAttribute('data-ability') || "");
+        });
+      }
+
+      // add border color
       if(ChatUtil.chatBorderColor===BORDER_COLOR_TYPES.playerColor.name && chatMessage.author?.id){ 
         elem.style.setProperty('border-color', `var(--user-color-${chatMessage.author.id})`);
       }
+      // Update the message document with the modified content
+      await chatMessage.update({
+        content: tempElement.innerHTML
+      });
   
     }   
+
+    LogUtil.log("enrichCard", [tempElement.firstChild]);
 
   }
 
